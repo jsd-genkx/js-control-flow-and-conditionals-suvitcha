052211339259b6689ecb577838bdb863e1030b02
suvitcha <boom.suvitcha@gmail.com>
2025-07-15 14:43:03 +0700

    task01 solution

--- task01.js
+++ task01.js
@@ -1,5 +1,11 @@
-"use strict"
+"use strict";
 
 // TODO: Use if/else to print "Even" or "Odd"
 
-const number = 15;
+const number = 14;
+console.log(number);
+if (number % 2==0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
